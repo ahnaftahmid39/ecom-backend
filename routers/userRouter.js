6449fd8d50b87e51af65c11cc6c21adb271bd7d5
Ahnaf <tahmidtahmid31@gmail.com>
2021-07-19 12:35:47 +0600

    adding purchase history

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { signIn, signUp } = require('../controllers/userControllers');
+const {
+  signIn,
+  signUp,
+  getPurchaseHistory,
+} = require('../controllers/userControllers');
+const authorize = require('../middlewares/authorize');
 
-router.route('/signup')
-    .post(signUp);
+router.route('/signup').post(signUp);
 
-router.route('/signin')
-    .post(signIn);
+router.route('/signin').post(signIn);
+router.route('/purchase-history').get(authorize, getPurchaseHistory);
 
 module.exports = router;
