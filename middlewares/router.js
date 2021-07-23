43891e628f28251a48d8c75971390db09967d74f
Ahnaf <tahmidtahmid31@gmail.com>
2021-07-23 10:36:58 +0600

    added facebook oAuth

--- middlewares/router.js
+++ middlewares/router.js
@@ -6,9 +6,11 @@ const profileRouter = require('../routers/profileRouter');
 const paymentRouter = require('../routers/paymentRouter');
 const couponRouter = require('../routers/couponRouter');
 const googleRouter = require('../routers/oauth/googleRouter');
+const facebookRouter = require('../routers/oauth/facebookRouter');
 
 module.exports = (app) => {
   app.use('/auth/google', googleRouter);
+  app.use('/auth/facebook', facebookRouter);
   app.use('/api/user', userRouter);
   app.use('/api/category', categoryRouter);
   app.use('/api/product', productRouter);
